1400f9d7143b5b9938f8647f98203799980d75ab
Carlos Sanchez <admroser@gmail.com>
2018-07-26 10:59:58 -0400

    catClicker V2

--- js/app.js
+++ js/app.js
@@ -11,28 +11,56 @@ Button to clear
 grid of squares
 
 */
-let imgClicked= document.querySelector('img');
+let catImg= document.getElementsByClassName('cat');
 let countClicks=document.querySelector('.clicks');
 let clicked=0;
+let catsName=document.querySelector('.catName');
+let cats={
+  names:["javi1", "javi2"]
 
-imgClicked.addEventListener("click", function () {
-  clicked+=clicked;
+
+};
+
+// for (name of cats.names ){
+
+// catsName.innerText=cats.names[name];
+
+// +=1;
+
+// }
+
+
+// console.log(cats[0].names);
+
+
+
+let catImages=[...catImg];
+
+
+for(let img of catImg){
+  img.addEventListener("click",  ()=> {
+   clicked+=1;
+   // if (catImg)
   countClicks.innerText="You clicked the cat "+clicked;
-
+ 
 });
 
 
-function makeGrid() {
-  const inputHeight = $('#inputHeight').val();
-  const inputWidth = $('#inputWidth').val();
 
-  for (let rows = 1; rows <= inputHeight; ++rows) {
-    $('#pixelCanvas').append($('<tr></tr>'));
-    for (let columns = 1; columns <= inputWidth; ++columns) {
-      $('tr').last().append($('<td></td>'));
-    }
-  }
 }
+
+
+// function makeGrid() {
+//   const inputHeight = $('#inputHeight').val();
+//   const inputWidth = $('#inputWidth').val();
+
+//   for (let rows = 1; rows <= inputHeight; ++rows) {
+//     $('#pixelCanvas').append($('<tr></tr>'));
+//     for (let columns = 1; columns <= inputWidth; ++columns) {
+//       $('tr').last().append($('<td></td>'));
+//     }
+//   }
+// }
 
 
 
